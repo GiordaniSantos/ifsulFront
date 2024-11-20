e98caa4be5a5239fa1871e4a6d6ee9be7ce297b4
Giordani <dani.santossilveira@hotmail.com>
2024-11-20 17:58:38 -0300

    add page login

--- src/app/(paginas)/login/page.tsx
+++ src/app/(paginas)/login/page.tsx
@@ -1,7 +1,48 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <div className="flex-1 flex flex-col container gap-5 py-10">
-            Pagina de Login
+        <div className="container" style={{display:"flex", justifyContent:"center", alignItems:"center", height: "85vh"}}>
+            <div className="row">
+                <div className="card" style={{width: 850, borderRadius: 25, backgroundColor: '#DCDCDC', border: 'none', marginTop: "-130px", boxShadow: "1px 2px"}}>
+                    <div className="row g-0">
+                        <div className="col-md-6" style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+                            <img src="/logo.png" className="img-fluid rounded-start" alt="..."/>
+                        </div>
+                        <div className="col-md-6" style={{padding: "20px 55px"}}>
+                            <div className="card-body">
+                                <h5 className="card-title mb-3">Login</h5>
+                                <form>
+                                    <div className="mb-3">
+                                        <label htmlFor="exampleInputEmail1" className="form-label">Email:</label>
+                                        <input type="email" className="form-control" id="exampleInputEmail1"
+                                               aria-describedby="emailHelp"/>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="exampleInputPassword1" className="form-label">Senha:</label>
+                                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                                    </div>
+                                    <button type="submit" className="btn btn-success mb-3"
+                                            style={{width: '100%'}}>Entrar
+                                    </button>
+                                    <Link href="/" style={{color: "#347B3E", textDecoration: "none"}}>
+                                        Esqueceu ou deseja alterar sua senha?
+                                    </Link>
+                                    <div className="container-text-center">
+                                        <div className="linha"></div>
+                                        <span>ou</span>
+                                        <div className="linha"></div>
+                                    </div>
+                                    Não possui uma conta?
+                                    <Link href="/" style={{marginLeft: "5px", color: "#198754", fontWeight: "bold", textDecoration: "none"}}>
+                                        Cadastre-se
+                                    </Link>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
