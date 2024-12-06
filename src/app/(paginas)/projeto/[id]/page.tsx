387857cb144f4cc9872cfb4f31a26c2d413030b6
Giordani da Silveira <dani.santossilveira@hotmail.com>
2024-12-06 09:32:11 -0300

    integrado detalhe do projeto com backend

--- src/app/(paginas)/projeto/[id]/page.tsx
+++ src/app/(paginas)/projeto/[id]/page.tsx
@@ -4,28 +4,49 @@ import Link from "next/link";
 import api from "@/Services/api";
 import { useRouter } from "next/navigation"; 
 import Swal from "sweetalert2";
+import { useEffect, useState } from "react";
 
 export default function Projeto({params,}: {params: { id: BigInteger };}) {
-    console.log(params.id)
+    
     const router = useRouter();
-    //const parametro = router.query.id
-    //console.log(parametro)
 
+    const [projeto, setProjeto] = useState<any>([]);
+
+    const buscarProjeto = async () => {
+        try {
+            
+            const resposta = await api.get(`/projetos/${params.id}`);
+            
+            setProjeto(resposta.data);
+        } catch (error:any) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: error.response.data
+            });
+        }
+    };
+    
+    useEffect(() => {
+        buscarProjeto();
+    }, []);
+    
     return (
         <div className="container" style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "85vh" }}>
             <div className="row">
                 <div className="card" style={{ width: 1000, padding:20, borderRadius: 20, backgroundColor: '#DCDCDC', border: 'none', marginTop: "-130px", boxShadow: "1px 2px" }}>
                     <div className="card-header" style={{ backgroundColor: '#DCDCDC'}}>
-                        <h3>Plataforma de Cursos Online <span className="badge bg-secondary">Idealização</span></h3>
+                        <h3>{projeto.nome} <span className="badge bg-secondary">Idealização</span></h3>
                     </div>
                     <div className="row g-0">
-                        <p>O projeto consiste no desenvolvimento de uma plataforma de cursos online voltada para fornecer aprendizado acessível e diversificado para um público amplo. A plataforma será desenvolvida com foco em usabilidade e acessibilidade, utilizando tecnologias modernas e uma arquitetura robusta para garantir uma experiência de navegação intuitiva e rápida.</p>
+                        <br />
+                        <p>{projeto.descricao}</p>
                         <h4>Funcionalidades Planejadas</h4>
-                        <p>
+                        <div>
                             <ul>
                                 <li>Cadastro de Usuários e Login</li>
                             </ul>
-                        </p>
+                        </div>
                         <h4>Tecnologias Utilizadas</h4>
                         <p>
                             <span className="badge bg-secondary me-2">PHP</span>
